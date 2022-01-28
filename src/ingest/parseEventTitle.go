7c22259e6f446116eee5577398a3e044935539f0
Kaspar Poland <kaspar78@mouco.com>
2022-01-28 08:43:10 -0500

    Rename parseCourseCode to parseEventTitle

--- src/ingest/parseEventTitle.go
+++ src/ingest/parseEventTitle.go
@@ -3,7 +3,7 @@ package ingest
 import "strings"
 
 func ParseEventTitle(summary string) string {
-	potentialMarkers := []string{"H1", "Y1"}
+	potentialMarkers := []string{"H1", "Y1", "H3", "Y3", "H5", "Y5"}
 
 	// Default to the entire string if no marker found
 	var index int
---
+++ src/ingest/parseEventTitle_test.go
@@ -0,0 +1,32 @@
+package ingest_test
+
+import (
+	"testing"
+
+	. "github.com/kaspar-p/bee/src/ingest"
+	"github.com/kaspar-p/bee/src/test"
+)
+
+func TestParseEventTitle(t *testing.T) {
+	t.Parallel()
+
+	type TestCase struct {
+		String   string
+		Expected string
+	}
+
+	tests := []TestCase{
+		{String: "MAT223H1 LEC0101", Expected: "MAT223"},
+		{String: "MAT223H3 LEC0101", Expected: "MAT223"},
+		{String: "MAT223H5 LEC0101", Expected: "MAT223"},
+		{String: "LIN200Y1 LEC0502", Expected: "LIN200"},
+		{String: "LIN200Y3 LEC0502", Expected: "LIN200"},
+		{String: "LIN200Y5 LEC0502", Expected: "LIN200"},
+		{String: "something else", Expected: "something else"},
+	}
+
+	for _, testCase := range tests {
+		actual := ParseEventTitle(testCase.String)
+		test.Equals(t, testCase.Expected, actual)
+	}
+}
